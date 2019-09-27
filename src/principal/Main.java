8db360d1f6ea0c7b545baa806a78ac812c5cb7cc
Jerson Ramírez Ortiz <juamkoo@gmail.com>
2019-09-27 04:12:37 -0500

    Agregando el archivo principal

---
+++ src/principal/Main.java
@@ -0,0 +1,14 @@
+package principal;
+
+import Controlador.escritorio.ControladorEscritorio;
+
+/**
+ *
+ * @author Jerson
+ */
+public class Main {
+    public static void main(String[] args) {
+        ControladorEscritorio cE = new ControladorEscritorio();
+        cE.open();
+    }
+}
