310ef9941b65a1880b5fd43f4528b39d224dda38
Jerson Ramírez Ortiz <juamkoo@gmail.com>
2019-10-20 20:11:31 -0500

    Modifico el archivo prueb.java

--- src/util/Prueba.java
+++ src/util/Prueba.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package util;
 
-import Controlador.escritorio.ControladorEscritorio;
 import conexionMysql.ConexionMysql;
 import enumerados.EDaoManager;
-import excepcion.NotAll;
 import interfaces.ICrud;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
